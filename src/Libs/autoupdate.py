3ec4630f36b6e0e73887ad607b85f3404afa1adb
Noah-Retro <noahschneider@outlook.de>
2023-12-13 23:48:53 +0100

    modified:   src/Libs/autoupdate.py

--- src/Libs/autoupdate.py
+++ src/Libs/autoupdate.py
@@ -6,7 +6,7 @@ def update():
     print("searching for changes in repo_")
     repo = git.Repo(os.getcwd())
     repo.remotes.origin.pull()
-    print("all up to date now")
+    print("all up to date")
     
 if __name__ == '__main__':
     print("updating")
