3bb5144e0fc199ec00a1a5d41463ab0e9f48a06b
Noah-Retro <noahschneider@outlook.de>
2023-12-14 01:21:20 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import src.Libs.autoupdate as ap
 from sys import platform
 import os
 
-if platform == "linux":
-    ap.update(os.path.dirname(os.path.abspath(__file__)))
+try:
+    if platform == "linux":
+        ap.update(os.path.dirname(os.path.abspath(__file__)))
+except:
+    pass
 
 
 #last test i hope
