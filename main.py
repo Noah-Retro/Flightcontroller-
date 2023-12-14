c55ea53b8260b159851a3b600284d0fc42670167
Noah-Retro <noahschneider@outlook.de>
2023-12-15 00:49:03 +0100

    modified:   main.py 	modified:   src/Libs/controller.py

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import src.Libs.autoupdate as ap
+import src.Libs.controller as controller
 from sys import platform
 import os
+from threading import Thread
 
 try:
     if platform == "linux":
         ap.update(os.path.dirname(os.path.abspath(__file__)))
 except Exception as e:
     raise e
+
+c = controller.PS4Controller()
+c.init()
+
+if __name__ == '__main__':
+    threads = []
+    controllerThread = Thread(target=c.listen,daemon=True)
+    threads.append(controllerThread)
+    
+    for thread in threads:
+        thread.start()
+        
+    while True:
+        print(c.button_data,c.axis_data,c.hat_data)
+
 
 
 
--- src/Libs/controller.py
+++ src/Libs/controller.py
@@ -2,6 +2,24 @@ import os
 import pprint
 import pygame
 
+"""
+0:"X"
+1:"O"
+2:Dreieck
+3:Quadrat
+4:L1
+5:R1
+6:L2
+7:R2
+8:share
+9:option
+10:PS button
+11:L3
+12:R3
+Axisdata:Axis on L and R Down {0,1} UP {0,-1} right {1,0} left {-1,0} data {{x,y},{x,y}} {L,R}
+hat_data:Dpad axis Up {0,1} Down {0,-1} right {1,0} left {-1,0}
+"""
+
 class PS4Controller(object):
     """Class representing the PS4 controller. Pretty straightforward functionality."""
 
@@ -48,11 +66,11 @@ class PS4Controller(object):
 
                 # Insert your code on what you would like to happen for each event here!
                 # In the current setup, I have the state simply printing out to the screen.
-                
+                return 
                 os.system('clear')
                 pprint.pprint(self.button_data)
-                pprint.pprint(self.axis_data)
-                pprint.pprint(self.hat_data)
+                pprint.pprint(self.axis_data.keys())
+                pprint.pprint(self.hat_data.keys())
 
 
 if __name__ == "__main__":
