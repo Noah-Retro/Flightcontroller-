1060995c545c84d6e45ebc1152d234a5e4e847c0
Noah-Retro <noahschneider@outlook.de>
2023-12-15 03:44:01 +0100

    modified:   src/Libs/autoupdate.py

--- src/Libs/autoupdate.py
+++ src/Libs/autoupdate.py
@@ -5,7 +5,7 @@ def update(path:str)->None:
     repo = git.Repo(path)
     fetch = repo.remotes.origin.pull()
     if fetch != None:
-        os.system('sudo shutdown -r now')
+        os.system('sudo reboot')
 
     
 if __name__ == '__main__':
