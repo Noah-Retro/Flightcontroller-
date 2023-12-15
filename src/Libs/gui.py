3696d167fbd2cd2b904a88bc46bb0fd637a4c7e2
Noah-Retro <noahschneider@outlook.de>
2023-12-15 16:03:47 +0100

    modified:   src/Libs/gui.py

--- src/Libs/gui.py
+++ src/Libs/gui.py
@@ -23,9 +23,11 @@ class App(threading.Thread):
         self.root.mainloop()
         
     def join(self):
+        
         self.root.destroy()
+        print("destroyed")
         super().join()
-        #
+        print("joint")
 
 
 
