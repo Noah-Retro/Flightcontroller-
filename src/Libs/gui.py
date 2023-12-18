0c9a79413944e13ef7dc6ecc926315541531ab63
Your Name <noahschneider@outlook.de>
2023-12-18 16:37:14 +0100

    modified:   src/Libs/test.py 	deleted:    src/Libs/th.py

--- src/Libs/gui.py
+++ src/Libs/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import threading
 
 
@@ -22,5 +23,37 @@ class App(threading.Thread):
         self.root.mainloop()
         
 
+class ControllerSettings(tk.ttk.Frame):
+    pass
+
+if __name__ == "__main__":
+    import tkinter as tk					 
+    from tkinter import ttk 
 
 
+    root = tk.Tk() 
+    root.title("Tab Widget") 
+    tabControl = ttk.Notebook(root) 
+
+    tab1 = ttk.Frame(tabControl) 
+    tab2 = ttk.Frame(tabControl) 
+
+    tabControl.add(tab1, text ='Tab 1') 
+    tabControl.add(tab2, text ='Tab 2') 
+    tabControl.pack(expand = 1, fill ="both") 
+
+    ttk.Label(tab1, 
+            text ="""Welcome to  
+            GeeksForGeeks""").grid(column = 0, 
+                                row = 0, 
+                                padx = 30, 
+                                pady = 30) 
+    ttk.Label(tab2, 
+            text ="""Lets dive into the
+            world of computers""").grid(column = 0, 
+                                        row = 0, 
+                                        padx = 30, 
+                                        pady = 30) 
+    ttk.OptionMenu(tab1,variable=["Hello","Hoi"],default="None")
+
+    root.mainloop() 
