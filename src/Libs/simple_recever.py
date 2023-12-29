eb5138446344c8f5b431e7132daebff49b7e575f
Noah-Retro <noahschneider@outlook.de>
2023-12-29 21:05:01 +0100

    s

--- src/Libs/simple_recever.py
+++ src/Libs/simple_recever.py
@@ -51,40 +51,40 @@ if __name__ == "__main__":
     
     # Display the content of NRF24L01 device registers.
     nrf.show_registers()
-    #
-    ## Enter a loop receiving data on the address specified.
-    #try:
-    #    print(f'Receive from {address}')
-    #    count = 0
-    #    while True:
-#
-    #        # As long as data is ready for processing, process it.
-    #        while nrf.data_ready():
-    #            # Count message and record time of reception.            
-    #            count += 1
-    #            now = datetime.now()
-    #            
-    #            # Read pipe and payload for message.
-    #            pipe = nrf.data_pipe()
-    #            payload = nrf.get_payload()    
-#
-    #            # Resolve protocol number.
-    #            protocol = payload[0] if len(payload) > 0 else -1            
-#
-    #            hex = ':'.join(f'{i:02x}' for i in payload)
-#
-    #            # Show message received as hex.
-    #            print(f"{now:%Y-%m-%d %H:%M:%S.%f}: pipe: {pipe}, len: {len(payload)}, bytes: {hex}, count: {count}")
-#
-    #            # If the length of the message is 9 bytes and the first byte is 0x01, then we try to interpret the bytes
-    #            # sent as an example message holding a temperature and humidity sent from the "simple-sender.py" program.
-    #            if len(payload) == 9 and payload[0] == 0x01:
-    #                values = struct.unpack("<Bff", payload)
-    #                print(f'Protocol: {values[0]}, temperature: {values[1]}, humidity: {values[2]}')
-    #            
-    #        # Sleep 100 ms.
-    #        time.sleep(0.1)
-    #except:
-    #    traceback.print_exc()
-    #    nrf.power_down()
-    #    pi.stop()
+    
+    # Enter a loop receiving data on the address specified.
+    try:
+        print(f'Receive from {address}')
+        count = 0
+        while True:
+        
+            # As long as data is ready for processing, process it.
+            while nrf.data_ready():
+                # Count message and record time of reception.            
+                count += 1
+                now = datetime.now()
+                
+                # Read pipe and payload for message.
+                pipe = nrf.data_pipe()
+                payload = nrf.get_payload()    
+    
+                # Resolve protocol number.
+                protocol = payload[0] if len(payload) > 0 else -1            
+    
+                hex = ':'.join(f'{i:02x}' for i in payload)
+    
+                # Show message received as hex.
+                print(f"{now:%Y-%m-%d %H:%M:%S.%f}: pipe: {pipe}, len: {len(payload)}, bytes: {hex}, count: {count}")
+    
+                # If the length of the message is 9 bytes and the first byte is 0x01, then we try to interpret the bytes
+                # sent as an example message holding a temperature and humidity sent from the "simple-sender.py" program.
+                if len(payload) == 9 and payload[0] == 0x01:
+                    values = struct.unpack("<Bff", payload)
+                    print(f'Protocol: {values[0]}, temperature: {values[1]}, humidity: {values[2]}')
+                
+            # Sleep 100 ms.
+            time.sleep(0.1)
+    except:
+        traceback.print_exc()
+        nrf.power_down()
+        pi.stop()
