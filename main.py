47be20a066fbc42e2a963b22f9dc771a8638f424
Noah-Retro <noahschneider@outlook.de>
2024-01-03 21:22:15 +0100

    main

--- main.py
+++ main.py
@@ -53,7 +53,8 @@ if __name__ == '__main__':
             for _ in range(controllerQueue.qsize()-1):
                 controllerData = controllerQueue.get_nowait()
         
-        
+        if not controllerQueueSend.empty():
+            print(controllerQueueSend.get_nowait())
         if len(controllerData)>0:
             if controllerData[0][0]:
                 controller.rumble(low_frequency=0.5,high_frequency=1,duration=0)
