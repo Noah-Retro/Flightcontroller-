20a49933a629a5cc81f369eb046fc2dc08af3ed0
Noah-Retro <noahschneider@outlook.de>
2024-01-03 08:23:08 +0100

    tx

--- main_airplane.py
+++ main_airplane.py
@@ -1,6 +1,7 @@
 import nrf24
 from src.Libs.transmition import Tx_Thread
 from threading import Thread
+import time
 
 data = 0
 tx = Tx_Thread()
@@ -10,3 +11,4 @@ print("Started")
 
 while True:
     print(data)
+    time.sleep(10)
