35b322c3f28419523f5a90b0fe39d5f7f976356a
Noah-Retro <noahschneider@outlook.de>
2024-01-06 13:40:57 +0100

    mpu

--- src/Libs/sensors/mpu.py
+++ src/Libs/sensors/mpu.py
@@ -43,11 +43,37 @@ class MPU_9250():
         self.imu.readSensor()
         self.imu.computeOrientation()
         return self.imu.yaw
+    
+    @property
+    def magVals(self):
+        self.imu.readSensor()
+        self.imu.computeOrientation()
+        return self.imu.MagVals
+    
+    def calliberate(self):
+        """Caliberate Accelerometer by positioning it in 6 different positions
+
+This function expects the user to keep the imu in 6 different positions while caliberation. It gives cues on when to change the position. It is expected that in all the 6 positions, at least one axis of IMU is parallel to gravity of earth and no position is same. Hence we get 6 positions namely -> +x, -x, +y, -y, +z, -z.
+        """
+        self.imu.caliberateAccelerometer()
+        self.imu.caliberateGyro()
+        
+    def calliberateMag(self):
+        self.imu.caliberateMagApprox()
 
 if __name__ == '__main__':
     sens = MPU_9250()
     sens.run()
+    sens.calliberateMag()
+    
     while True:
         print("Accelometer ",sens.accelVal)
         print("Gyro ", sens.gyroVal)
         print(f"Roll: {sens.roll}, Pitch: {sens.pitch}, Yaw: {sens.yaw}")
+        print("Mag vals: ",sens.magVals)
+    """
+    Output:
+    Accelometer  [-0.18675724 -0.13408212 -9.48631028]
+    Gyro  [ 0.00196677 -0.0028696   0.00085888]
+    Roll: -178.7302714712781, Pitch: 0.8388532267318494, Yaw: nan
+    """
