56899501d5a0ac287fd4ee5f7cd06d0fc085132c
Noah-Retro <noahschneider@outlook.de>
2024-01-09 19:05:54 +0100

    Debug Leds

--- src/Libs/tools.py
+++ src/Libs/tools.py
@@ -1,4 +1,6 @@
 import json
+import neopixel
+import board
 
 class Status():
     NOSTATE=[0,0,0]
@@ -12,19 +14,21 @@ class Status():
     TRANSMITTERROR=[255,0,0]
     DBERROR=[0,0,255]
     SETTINGSERROR=[0,255,0]
-        
-
-
+       
+class LEDS():   
+    DATALED=0
+    HEALTHLED=1
+    PROGLED=2
 
 class DebugLEDHandler():
-    def __init__(pin:int=19):
-        #init WS2018 Led strip
-        pass
-
-    @staticmethod
-    def progLed(state:Status,place:int,pin:int=19):
-        #write status to led
-        pass
+    def __init__(self):
+        self.pixel = neopixel.NeoPixel(board.D19,3)
+        
+    def progLed(self,state:Status,place:int,pin:int=19):
+        self.pixel[place]=tuple(state)
+        self.pixel.show()
 
 if __name__ == "__main__":
     print(tuple(Status.I2CERROR))
+    DLed = DebugLEDHandler()
+    DLed.progLed(Status.UNKNOWNERROR,LEDS.DATALED)
