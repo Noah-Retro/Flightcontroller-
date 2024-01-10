2b4d3a1868b0bf37f771d1d38ecfa350af80e241
Noah-Retro <noahschneider@outlook.de>
2024-01-10 20:04:31 +0100

    test

--- src/Libs/actors/servo.py
+++ src/Libs/actors/servo.py
@@ -12,46 +12,62 @@ import time
 import random
 import pigpio
 
+NUM_GPIO=32
 
+MIN_WIDTH=1000
+MAX_WIDTH=2000
+
+step = [0]*NUM_GPIO
+width = [0]*NUM_GPIO
+used = [False]*NUM_GPIO
 
 pi = pigpio.pi()
 
 if not pi.connected:
    exit()
 
-def scale(ins:float,_min:int=950,_max:int=2100):
-    res = (_max-_min) * ins + _min
-    return int(res)
-    
+if len(sys.argv) == 1:
+   G = [4]
+else:
+   G = []
+   for a in sys.argv[1:]:
+      G.append(int(a))
+   
+for g in G:
+   used[g] = True
+   step[g] = random.randrange(5, 25)
+   if step[g] % 2 == 0:
+      step[g] = -step[g]
+   width[g] = random.randrange(MIN_WIDTH, MAX_WIDTH+1)
 
-class CustomServo():
-    def __init__(self,pin:int) -> None:
-        self.pin = pin
-        self.MIN_WIDTH=950
-        self.MAX_WIDTH=2100
-        
-    def setVal(self,ins:int):
-        print(ins)
-        pi.set_servo_pulsewidth(self.pin,scale(ins))
-        
-    def stop(self):
-        pi.set_servo_pulsewidth(self.pin,0)
+print("Sending servos pulses to GPIO {}, control C to stop.".
+   format(' '.join(str(g) for g in G)))
 
-if __name__=="__main__":
-    servof = CustomServo(17)
-    while True:
-        try:
-            for i in range(100):
-                r = (i+ 0.01)/100 
-                print(r)
-                print(scale(r))
-                servof.setVal(scale(r))
-                time.sleep(0.1)
+while True:
 
-        except KeyboardInterrupt:
-            pi.set_servo_pulsewidth(17, 0)
-            pi.stop()
+   try:
 
+      for g in G:
 
+         pi.set_servo_pulsewidth(g, 950)
 
+         # print(g, width[g])
+
+         width[g] += step[g]
+
+         if width[g]<MIN_WIDTH or width[g]>MAX_WIDTH:
+            step[g] = -step[g]
+            width[g] += step[g]
+
+      time.sleep(0.1)
+
+   except KeyboardInterrupt:
+      break
+
+print("\nTidying up")
+
+for g in G:
+   pi.set_servo_pulsewidth(g, 0)
+
+pi.stop()
 
