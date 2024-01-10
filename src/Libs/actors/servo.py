05b5a94725ad2d682131705455109b4eddda5f90
Noah-Retro <noahschneider@outlook.de>
2024-01-10 18:54:26 +0100

    test

--- src/Libs/actors/servo.py
+++ src/Libs/actors/servo.py
@@ -5,15 +5,22 @@ servoPIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
+def setAngle(angle):
+    duty = angle / 18 + 2
+    GPIO.output(03, True)
+    p.ChangeDutyCycle(duty)
+    time.sleep(1)
+    GPIO.output(03, False)
+    p.ChangeDutyCycle(0)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 als PWM mit 50Hz
 p.start(4) # Initialisierung
 try:
   while True:
-    p.ChangeDutyCycle(4)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
+    setAngle(0)
+    setAngle(90)
+    setAngle(180)
+    
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
