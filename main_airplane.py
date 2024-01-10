ddbbbcfb120c41fbecb0bc9222efa012b6fb5ff5
Noah-Retro <noahschneider@outlook.de>
2024-01-10 02:38:28 +0100

    debug

--- main_airplane.py
+++ main_airplane.py
@@ -5,34 +5,45 @@ import time
 from queue import Queue
 import os
 import sys
+import sqlite3
 from src.Libs.db_tools import DbHandler
 from src.Libs.tools import DebugLEDHandler,LEDS,Status
 from src.Libs.sensors import MPU_9250
 
 print(os.path.abspath('.'))
 
+
+
+dled = DebugLEDHandler()
+dled.progLed(Status.BOOTUP,LEDS.HEALTHLED)
+
 button_queue = Queue()
 axis_queue = Queue()
 tx = Rx_Thread(button_queue=button_queue,axis_queue=axis_queue)
-#print("Started thread in 1")
 tx.start()
-#print("Started")
 
-dled = DebugLEDHandler()
 mpu = MPU_9250()
-db = DbHandler()
 mpu.run()
+
+try:
+    db = DbHandler()
+except sqlite3.OperationalError as e:
+    dled.progLed(Status.DBERROR,LEDS.DATALED)
+    
 
 while True: 
     try:
-        db.storeMPUData(mpu.dataFrame)
-    except Exception as e:
-        dled.progLed(Status.DBERROR,LEDS.DATALED)
-    if not button_queue.empty():
-        print(button_queue.get_nowait())
-    if not axis_queue.empty():
-        print(axis_queue.get_nowait())
-    else:
-        dled.progLed(Status.TRANSMITTERROR,LEDS.DATALED)
+        try:
+            db.storeMPUData(mpu.dataFrame)
+        except Exception as e:
+            dled.progLed(Status.DBERROR,LEDS.DATALED)
+        if not button_queue.empty():
+            print(button_queue.get_nowait())
+        if not axis_queue.empty():
+            print(axis_queue.get_nowait())
+        else:
+            dled.progLed(Status.TRANSMITTERROR,LEDS.DATALED)
+    except KeyboardInterrupt:
+        dled.clear()
     
 
