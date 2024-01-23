7a901e8af45881fa8649429f8f609a063d57436a
Noah-Retro <noahschneider@outlook.de>
2024-01-23 19:40:11 +0100

    test

--- src/Libs/actors/servo.py
+++ src/Libs/actors/servo.py
@@ -55,7 +55,7 @@ class CustomServo(Motor):
 class CustomBrushless(Motor):
     def __init__(self,pin:int,clamp_min:float=0,clamp_max:float=2) -> None:
         self.pin = pin
-        self.MIN_WIDTH=700
+        self.MIN_WIDTH=0
         self.MAX_WIDTH=2000
         self.clamp_min = clamp_min
         self.clamp_max = clamp_max
@@ -69,9 +69,9 @@ class CustomBrushless(Motor):
         pi.set_servo_pulsewidth(self.pin, 0)
         time.sleep(2)
         pi.set_servo_pulsewidth(self.pin, self.MAX_WIDTH)
-        time.sleep(2)
+        time.sleep(10)
         pi.set_servo_pulsewidth(self.pin, self.MIN_WIDTH)
-        time.sleep(2)
+        time.sleep(10)
      
 
 if __name__=="__main__":
