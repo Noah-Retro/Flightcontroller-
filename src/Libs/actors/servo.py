eb7c4f6395a29018910e7f3f36869035b8bc380b
Noah-Retro <noahschneider@outlook.de>
2024-01-23 19:03:53 +0100

    throttle test

--- src/Libs/actors/servo.py
+++ src/Libs/actors/servo.py
@@ -49,14 +49,14 @@ class CustomServo(Motor):
         self.zero = zero
     
     def scale(self,ins:float):
-        res = (self.MAX_WIDTH-self.MIN_WIDTH) * (np.clip((ins+1+(self.zero-1)),self.clamp_min,self.clamp_max)/(self.clamp_max)) + self.MIN_WIDTH        
+        res = (self.MAX_WIDTH-self.MIN_WIDTH) * (np.clip((ins+(self.zero)),self.clamp_min,self.clamp_max)/(self.clamp_max)) + self.MIN_WIDTH        
         return int(res)
 
 class CustomBrushless(Motor):
     def __init__(self,pin:int,clamp_min:float=0,clamp_max:float=2) -> None:
         self.pin = pin
-        self.MIN_WIDTH=950
-        self.MAX_WIDTH=2100
+        self.MIN_WIDTH=700
+        self.MAX_WIDTH=2000
         self.clamp_min = clamp_min
         self.clamp_max = clamp_max
 
--- test.py
+++ test.py
@@ -1,6 +1,21 @@
-from src.Libs.gui import App
+import numpy as np
 
-a = App("Flightcontroller-/src/settings")
+class CustomBrushless():
+    def __init__(self,pin:int,clamp_min:float=0,clamp_max:float=2) -> None:
+        self.pin = pin
+        self.MIN_WIDTH=950
+        self.MAX_WIDTH=2100
+        self.clamp_min = clamp_min
+        self.clamp_max = clamp_max
+        self.zero=0
 
-a.run()
+    def scale(self,ins:float):
+        res = (self.MAX_WIDTH-self.MIN_WIDTH) * (np.clip((ins+(self.zero)),self.clamp_min,self.clamp_max)/(self.clamp_max)) + self.MIN_WIDTH        
+        return int(res)
+    
+if __name__ == "__main__":
+    br = CustomBrushless(26,0,2)
+    listd = [-1,-0.5,0,0.5,1]
 
+    for i in listd:
+        print(br.scale(i))
