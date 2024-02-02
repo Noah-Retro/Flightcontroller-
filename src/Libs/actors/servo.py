2fa4977ad5a11160e59bc9e7f717d1e743c7a9bf
Your Name <noahschneider@outlook.de>
2024-02-02 17:03:22 +0100

    test

--- src/Libs/actors/servo.py
+++ src/Libs/actors/servo.py
@@ -62,8 +62,6 @@ class CustomBrushless(Motor):
 
     def scale(self,ins:float):
         res = (self.MAX_WIDTH-self.MIN_WIDTH) * (np.clip((ins),self.clamp_min,self.clamp_max)/(self.clamp_max)) + self.MIN_WIDTH        
-        if res <= 750:
-            res = 0
         return int(res) 
 
     def arm(self): #This is the arming procedure of an ESC 
