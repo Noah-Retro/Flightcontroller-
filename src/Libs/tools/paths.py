a67872431460761a37235be66abe486cd3538453
Noah-Retro <noahschneider@outlook.de>
2024-02-07 00:04:16 +0100

    test

--- src/Libs/tools/paths.py
+++ src/Libs/tools/paths.py
@@ -1,9 +1,9 @@
 import os
 
 CONTROLLER_SETTINGS_PATH = os.path.abspath("src/settings/controller.json")
-DATA_SETTINGS_PATH = os.path.abspath("src/settings/controller.json")
-DEBUG_LED_PATH = os.path.abspath("src/settings/controller.json")
-MOTORS_SETTINGS_PATH = os.path.abspath("src/settings/controller.json")
-TRANSMITT_SETTINGS_PATH = os.path.abspath("src/settings/controller.json")
-VIDEO_SETTINGS_PATH = os.path.abspath("src/settings/controller.json")
+DATA_SETTINGS_PATH = os.path.abspath("src/settings/data.json")
+DEBUG_LED_PATH = os.path.abspath("src/settings/debug.json")
+MOTORS_SETTINGS_PATH = os.path.abspath("src/settings/motors.json")
+TRANSMITT_SETTINGS_PATH = os.path.abspath("src/settings/transmitt.json")
+VIDEO_SETTINGS_PATH = os.path.abspath("src/settings/video.json")
 SETTINGS_PATH = os.path.abspath("src/settings")
