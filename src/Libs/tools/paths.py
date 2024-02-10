718aabfec6b03d12061e764ac2c116c4657ac594
Noah-Retro <noahschneider@outlook.de>
2024-02-11 00:56:55 +0100

    test

--- src/Libs/tools/paths.py
+++ src/Libs/tools/paths.py
@@ -1,5 +1,7 @@
 import os
+from pathlib import Path
 
+ROOT_DIR = Path(__file__).parent
 CONTROLLER_SETTINGS_PATH = os.path.abspath("src/settings/controller.json")
 DATA_SETTINGS_PATH = os.path.abspath("src/settings/data.json")
 DEBUG_LED_PATH = os.path.abspath("src/settings/debug.json")
@@ -7,5 +9,4 @@ MOTORS_SETTINGS_PATH = os.path.abspath("src/settings/motors.json")
 TRANSMITT_SETTINGS_PATH = os.path.abspath("src/settings/transmitt.json")
 VIDEO_SETTINGS_PATH = os.path.abspath("src/settings/video.json")
 SETTINGS_PATH = os.path.abspath("src/settings")
-
-print(CONTROLLER_SETTINGS_PATH)
+print(ROOT_DIR)
