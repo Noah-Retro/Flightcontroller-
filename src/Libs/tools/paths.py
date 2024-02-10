0106efbb53f42cd9681f2dd3fbecfaa2473871e3
Noah-Retro <noahschneider@outlook.de>
2024-02-11 00:58:38 +0100

    test

--- src/Libs/tools/paths.py
+++ src/Libs/tools/paths.py
@@ -2,11 +2,11 @@ import os
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent.parent.parent
-CONTROLLER_SETTINGS_PATH = os.path.abspath("src/settings/controller.json")
-DATA_SETTINGS_PATH = os.path.abspath("src/settings/data.json")
-DEBUG_LED_PATH = os.path.abspath("src/settings/debug.json")
-MOTORS_SETTINGS_PATH = os.path.abspath("src/settings/motors.json")
-TRANSMITT_SETTINGS_PATH = os.path.abspath("src/settings/transmitt.json")
-VIDEO_SETTINGS_PATH = os.path.abspath("src/settings/video.json")
-SETTINGS_PATH = os.path.abspath("src/settings")
+CONTROLLER_SETTINGS_PATH = ROOT_DIR+"src/settings/controller.json"
+DATA_SETTINGS_PATH = ROOT_DIR+"src/settings/data.json"
+DEBUG_LED_PATH = ROOT_DIR+"src/settings/debug.json"
+MOTORS_SETTINGS_PATH = ROOT_DIR+"src/settings/motors.json"
+TRANSMITT_SETTINGS_PATH = ROOT_DIR+"src/settings/transmitt.json"
+VIDEO_SETTINGS_PATH = ROOT_DIR+"src/settings/video.json"
+SETTINGS_PATH = ROOT_DIR+"src/settings"
 print(ROOT_DIR)
